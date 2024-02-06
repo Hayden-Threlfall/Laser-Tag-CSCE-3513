9e6a00532edd3001d710aaf1564d23677e557896
Hayden-Threlfall <Hayden.t.threlfall@gmail.com>
2024-02-06 14:32:04 -0600

    fix

--- src/Events.java
+++ src/Events.java
@@ -1,6 +1,6 @@
 public class Events {
     public void event(String msg) {
-        //UDPSend.send(ID)); send out
+        //UDPSend.send(ID); send out
         //Scores.green.put(ID, 0); initilaizing
         //Scores.green.compute(ID, (key, val) -> val+=10);
     }
