08ade06de83b6df3bce0f30887570bd337280c1e
Hayden-Threlfall <Hayden.t.threlfall@gmail.com>
2024-02-06 14:31:19 -0600

    Created Template Scores and Events

---
+++ src/Events.java
@@ -0,0 +1,7 @@
+public class Events {
+    public void event(String msg) {
+        //UDPSend.send(ID)); send out
+        //Scores.green.put(ID, 0); initilaizing
+        //Scores.green.compute(ID, (key, val) -> val+=10);
+    }
+}
---
+++ src/Scores.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class Scores {
+    //HashMap<     ID    , Player_Score    >
+    public HashMap<String, Integer> green = new HashMap<>();
+    public HashMap<String, Integer> red = new HashMap<>();
+
+    //HashMap<     ID    , Username    >
+    //public HashMap<String, String> usernames = new HashMap<>();
+}
