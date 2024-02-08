0caa99ddf3e4f09a8e6e5439d932255b2971df34
JohnDog3112 <johndog3112@gmail.com>
2024-02-08 17:00:00 -0600

    Commented HTTPServer.java

--- src/HTTP/HTTPServer.java
+++ src/HTTP/HTTPServer.java
@@ -11,11 +11,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Scanner;
@@ -31,21 +29,31 @@ public class HTTPServer {
         server.start();
         System.out.println("Server started on port " + serverPort);
 
+        //get all files within the web directory
         File[] files = (new File(WEB_DIR)).listFiles();
+        //convert the array of files into a queue
         Queue<File> fileQueue = new LinkedList<>(Arrays.asList(files));
         
+        //every iteration, take the next file out of the queue
+        //continue until queue is empty
         while (!fileQueue.isEmpty()) {
+            //get next file
             File file = fileQueue.remove();
+            //if it's a directory, add all of it's sub files/directories
+            //  into the queue to be processed
             if (file.isDirectory()) {
                 for (File sub_file : file.listFiles()) {
                     fileQueue.add(sub_file);
                 }
             } else {
+                //cut of the WEB_DIR/... part of the file path
                 String path = file.getPath().substring(WEB_DIR.length());
+                //create new handler for the request
                 StaticFile handler = new StaticFile(file);
+                //create the context at the path
                 server.createContext(path, handler);
 
-                System.out.println("Name: " + file.getName());
+                //if it's index.html, also bind it to / so you don't need /index.html
                 if (file.getName().equals("index.html")) {
                     String rootPath = path.substring(0, path.length() - "index.html".length());
                     System.out.println("root path: " + rootPath);
@@ -58,22 +66,28 @@ public class HTTPServer {
     
 
     static class StaticFile implements HttpHandler {
+        //mapping .<type> to the Content-Type
         private static final Map<String, String> TYPES = new HashMap<String, String>() {{
             put("html", "text/html");
             put("htm", "text/html");
             put("css", "text/css");
             put("js", "text/javascript");
             put("ico", "image/vnd.microsoft.icon");
-        }}; 
+        }};
+        //string of file data
         private final String data;
+        //the content-type
         private final String type;
 
+        //whether it failed to load the file
         private final boolean failed;
 
         StaticFile(File file) {   
+            //get the file type
             String[] split = file.getName().split("\\.");
             String type = split[split.length-1];
 
+            //map it to the content-type
             if (!TYPES.containsKey(type)) {
                 System.out.println("Unknown file type! " + type);
                 this.type = "text/plain";
@@ -84,6 +98,7 @@ public class HTTPServer {
             String data = "";
             boolean failed = false;
             try {
+                //read in all of the file data
                 Scanner fileReader = new Scanner(file);
                 while (fileReader.hasNextLine()) {
                     data += fileReader.nextLine() + "\n";
@@ -101,8 +116,11 @@ public class HTTPServer {
 
         @Override
         public void handle(HttpExchange exchange) throws IOException {
+            //if it failed, just return 404 (should probably return better error message)
             if (this.failed) {
                 exchange.sendResponseHeaders(404, -1);
+            
+            //for get requests, simply return the file data
             } else if ("GET".equals(exchange.getRequestMethod())) {
 
                 // Send a JSON response
@@ -112,7 +130,7 @@ public class HTTPServer {
                 os.write(this.data.getBytes());
                 os.close();
             } else {
-                // Method not allowed
+                // for other types, send invalid method
                 exchange.sendResponseHeaders(405, -1);
             }
         }
