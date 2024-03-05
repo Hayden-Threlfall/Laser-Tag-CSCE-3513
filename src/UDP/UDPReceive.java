584e3cdf09e3771e7a163d2f871a41a27992e705
Hayden-Threlfall <Hayden.t.threlfall@gmail.com>
2024-03-05 14:10:15 -0600

    Reverting UDPReceive

--- src/UDP/UDPReceive.java
+++ src/UDP/UDPReceive.java
@@ -8,8 +8,6 @@ public class UDPReceive extends Thread{
     private final static int PORT = 7501;
     private Scoring Scores;
 
-    private boolean running = false;
-
     public UDPReceive(Scoring s) {
         Scores = s;
     }
@@ -21,11 +19,7 @@ public class UDPReceive extends Thread{
 
             System.out.println("UDP Server is listening on port " + PORT);
 
-            running = true;
-            while (running) {
-                if (!running) {
-                    break;
-                }
+            while (true) {
                 // Create a buffer to hold incoming data
                 byte[] buffer = new byte[1024];
 
@@ -36,7 +30,6 @@ public class UDPReceive extends Thread{
                 socket.receive(packet);
                 
                 socket.close();
-
 
                 // Extract the data from the packet
                 String receivedData = new String(packet.getData(), 0, packet.getLength());
@@ -51,9 +44,64 @@ public class UDPReceive extends Thread{
             e.printStackTrace();
         }
     }
-
-    public void stop_processing() {
-        running = false;
-        this.interrupt();
-    }
 }
+// OLD CODE
+// package UDP;
+
+// import UDP.Scoring;
+// import java.net.DatagramPacket;
+// import java.net.DatagramSocket;
+
+// public class UDPReceive extends Thread{
+//     private final static int PORT = 7501;
+//     private Scoring Scores;
+
+//     private boolean running = false;
+
+//     public UDPReceive(Scoring s) {
+//         Scores = s;
+//     }
+
+//     public void run() {
+//         try {
+//             // Create a DatagramSocket to listen for UDP packets on the specified port
+//             DatagramSocket socket = new DatagramSocket(PORT);
+
+//             System.out.println("UDP Server is listening on port " + PORT);
+
+//             running = true;
+//             while (running) {
+//                 if (!running) {
+//                     break;
+//                 }
+//                 // Create a buffer to hold incoming data
+//                 byte[] buffer = new byte[1024];
+
+//                 // Create a DatagramPacket to receive the incoming UDP packet
+//                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+//                 // Receive the UDP packet
+//                 socket.receive(packet);
+                
+//                 socket.close();
+
+
+//                 // Extract the data from the packet
+//                 String receivedData = new String(packet.getData(), 0, packet.getLength());
+
+//                 // Display the received data
+//                 //System.out.println("Received UDP packet from " + packet.getAddress() + ":" + packet.getPort());
+//                 System.out.println("Data: " + receivedData);
+//                 Scores.update(receivedData);
+//             }
+
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
+//     }
+
+//     public void stop_processing() {
+//         running = false;
+//         this.interrupt();
+//     }
+// }
