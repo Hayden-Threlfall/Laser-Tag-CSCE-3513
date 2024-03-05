04bd58dcd132e3af3c978f9502cf15004e739c98
Hayden-Threlfall <Hayden.t.threlfall@gmail.com>
2024-03-05 15:11:32 -0600

    UDP Test Bash updated

--- src/UDP/UDPMessage.bash
+++ src/UDP/UDPMessage.bash
@@ -5,7 +5,11 @@ TARGET_IP="192.168.137.142"
 TARGET_PORT="7501"
 
 # Set the message to be sent
-MESSAGE="2:5"
+#MESSAGE="5:2" # 2 Hit 5
+#MESSAGE="2:5"
+MESSAGE="10:5" # 2 Hit self
+#MESSAGE="2:43" # Capture Green Base
+#MESSAGE="5:53" # Capture Red base
 
 # Use echo to send the UDP message without netcat
 echo -n "$MESSAGE" > /dev/udp/$TARGET_IP/$TARGET_PORT
