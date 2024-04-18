55de0399217b9007bf4169d5dc369c84aedf481d
Hayden-Threlfall <Hayden.t.threlfall@gmail.com>
2024-04-18 12:27:56 -0500

    Run.sh checks if in windows
    the run.sh now works for windows or linux no need for run.ps1

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 cd src
 
-export CLASSPATH=.:../lib/*
+if [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    export CLASSPATH=".;../lib/*"
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    export CLASSPATH=".;../lib/*"
+else
+    export CLASSPATH=.:../lib/*
+fi
 
 javac Main.java
 
